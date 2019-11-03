cecd5f1b0d32ec87fbbf3cf84de7fae87d7deb94
CJChen98 <44677054+CJChen98@users.noreply.github.com>
2019-11-03 11:07:03 +0800

    调用LifCycleObserver接口

--- app/src/main/java/com/example/lifecycles/MainActivity.java
+++ app/src/main/java/com/example/lifecycles/MainActivity.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    MyChronometer chronometer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        chronometer=findViewById(R.id.chronometer);
+        getLifecycle().addObserver(chronometer);
     }
 }
